515941666506fb25c0405d0e773ad790b5511d6c
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-08-12 21:01:39 +0800

    Update getvless.py

--- utils/getvless.py
+++ utils/getvless.py
@@ -35,6 +35,7 @@ def decrypt_aes(data):
     #decrypted_data = unpad(cipher.decrypt(base64.b64decode(data)), AES.block_size)
     decrypted_data = unpad(cipher.decrypt(a2b_hex(data)), AES.block_size)
     strdecrypted_datastr = str(decrypted_data, encoding = "utf-8")
+    #print(strdecrypted_datastr)
     #print(strdecrypted_datastr.encode('utf8').decode('unicode_escape'))
     return strdecrypted_datastr.encode('utf8')
 
