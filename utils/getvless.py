1d23b2e4e5362edd3d137dfd3e48e4b7c409ef4c
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-08-12 18:56:25 +0800

    Update getvless.py

--- utils/getvless.py
+++ utils/getvless.py
@@ -37,8 +37,8 @@ def get_node():
     headers = {"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 10; MI 9 MIUI/20.9.4)"}
     req = requests.get(url, headers=headers, verify=False)
     encrypted_data = str(req)
-    print(encrypted_data)
-    node_list = json.loads(str(decrypt_aes(encrypted_data)))['title']
+    print(req)
+    node_list = json.loads(str(decrypt_aes(req)))['title']
     Vless = ''
     for i in node_list :
         host = i['ip']
