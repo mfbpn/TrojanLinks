94de9b6db6436e006fc808392152cd0aa03ac1c5
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-08-12 11:11:06 +0800

    Update getvless.py

--- utils/getvless.py
+++ utils/getvless.py
@@ -23,38 +23,6 @@ from Telegram_bot import send_message
 urllib3.disable_warnings()
 
 
-def invite():
-    url = f'{api}/addRefereeToUserReferral/'
-    headers = {
-        'accept': 'application/json',
-        'accept-charset': 'UTF-8',
-        'cache-control': 'max-age=1800',
-        'user-agent': 'Ktor client',
-        'content-type': 'application/x-www-form-urlencoded',
-        'content-length': '0',
-        'accept-encoding': 'gzip'
-
-    }
-    number = 0
-    while number < 3:
-        Id = uuid.uuid4()
-        data = {
-            'uniqueId': Id,
-            'referralCode': 'D4GOLG'
-        }
-        req = requests.post(url, data=data, headers=headers, verify=False)
-        print(req.text)
-        number += 1
-        time.sleep(3)
-
-
-def decrypt_rsa(data):
-    key = RSA.importKey(base64.urlsafe_b64decode(private_key))
-    cipher = PKCS1_OAEP.new(key)
-    decrypted_message = cipher.decrypt(base64.b64decode(data))
-    return decrypted_message.decode()
-
-
 def decrypt_aes(data):
     key = private_key
     iv = authorization
@@ -67,32 +35,16 @@ def get_node():
     url = api
     headers = {"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 10; MI 9 MIUI/20.9.4)"}
     req = requests.get(url, headers=header)
-    node_info = decrypt_aes(req.text)
+    node_info = json.loads(decrypt_aes(req.text))['goserverlist']
     Vless = ''
-    for server_list in json.loads(node_info):
-        if server_list['servers']:
-            for servers in server_list['servers']:
-                if servers:
-                    server = servers['server']
-                    ip = servers['ip']
-                    if ip:
-                        url = f'https://ip125.com/api/{ip}?lang=zh-CN'
-                        head = {
-                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
-                            'Cookie': '_ga=GA1.2.348223593.1668760697; _ga_XYJPKQNDKR=GS1.1.1669809730.4.1.1669809752.0.0.0; __gads=ID=e9cb6076c0188403-228d0f367edf00b9:T=1683097828:RT=1701660864:S=ALNI_MZoNQcRpP-66ZZidp6BAlct92mbOw; __gpi=UID=00000c011afd3f29:T=1683097828:RT=1701660864:S=ALNI_MZSTguCSNwyc6d4WgMIcm7m-Xepvg'
-                        }
-                        country_info = requests.get(url, headers=head).json()
-                        address = country_info['country'] + country_info['city']
-                        vless = 'ss://YWVzLTI1Ni1jZmI6YW1hem9uc2tyMDU=' + '@' + ip + ':' + '443' + '#' + '%F0%9F%87%AD%F0%9F%87%B0%20%F0%9D%99%8F%F0%9D%99%82%40%F0%9D%99%88%F0%9D%99%81%F0%9D%98%BD%F0%9D%99%8B%F0%9D%99%89%200'
-                        Vless += vless + '\n'
-                    else:
-                        print(server)
-                else:
-                    print(servers)
-        else:
-            print(server_list)
+    for i in node_info :
+        host = i['ip']
+        vless = 'ss://YWVzLTI1Ni1jZmI6YW1hem9uc2tyMDU=' + '@' + host + ':' + '443' + '#' + '%F0%9F%87%AD%F0%9F%87%B0%20%F0%9D%99%8F%F0%9D%99%82%40%F0%9D%99%88%F0%9D%99%81%F0%9D%98%BD%F0%9D%99%8B%F0%9D%99%89%200'
+        Vless += vless + '\n'
+        if i == node_info[11]:
+            break
     with open("./links/vless", "w") as f:
-        f.write(base64.b64encode(Vless.encode()).decode())
+    f.write(base64.b64encode(Vless.encode()).decode())
 
 
 if __name__ == '__main__':
