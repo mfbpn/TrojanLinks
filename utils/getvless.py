4d2d273bec861a9d9ad1b855934b6664406d0cda
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-08-12 12:46:35 +0800

    Update getvless.py

--- utils/getvless.py
+++ utils/getvless.py
@@ -61,6 +61,7 @@ def get_node():
     url = api
     headers = {"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 10; MI 9 MIUI/20.9.4)"}
     req = requests.get(url, headers=headers, verify=False).text
+    print(req)
     node_list = json.loads(str(decode(req)))['title']
     Vless = ''
     for i in node_list :
