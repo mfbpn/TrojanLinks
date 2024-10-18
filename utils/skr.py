04db082209697c78697774c770eaacf6cb6abf5f
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-10-18 19:12:51 +0800

    Update skr.py

--- utils/skr.py
+++ utils/skr.py
@@ -2,12 +2,12 @@ import os
 import time
 import uuid
 import urllib3
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Cipher import AES
-from Crypto.Hash import MD5
-from Crypto.Util.Padding import pad, unpad
-from binascii import a2b_hex, b2a_hex
+# from Crypto.PublicKey import RSA
+# from Crypto.Cipher import PKCS1_OAEP
+# from Crypto.Cipher import AES
+# from Crypto.Hash import MD5
+# from Crypto.Util.Padding import pad, unpad
+# from binascii import a2b_hex, b2a_hex
 from base64 import b64decode
 from Telegram_bot import send_message
 
@@ -18,26 +18,19 @@ import pyaes
 import binascii
 from datetime import datetime
 
-
 if __name__ == '__main__':
-    # a = 'http://api.skrapp.net/api/serverlist'
     a = os.environ['skr_a']
-    # b = {
-    #     'accept': '/',
-    #     'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
-    #     'appversion': '1.3.1',
-    #     'user-agent': 'SkrKK/1.3.1 (iPhone; iOS 13.5; Scale/2.00)',
-    #     'content-type': 'application/x-www-form-urlencoded',
-    #     'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
-    # }
-    b = os.environ['skr_b']
-    # c = {'data': '4265a9c353cd8624fd2bc7b5d75d2f18b1b5e66ccd37e2dfa628bcb8f73db2f14ba98bc6a1d8d0d1c7ff1ef0823b11264d0addaba2bd6a30bdefe06f4ba994ed'}
     c = os.environ['skr_c']
-    # d = b'65151f8d966bf596'
     d = os.environ['skr_d']
     e = os.environ['skr_e']
-    # e = b'88ca0f0ea1ecf975'
-
+    b = {
+        'accept': '/',
+        'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+        'appversion': '1.3.1',
+        'user-agent': 'SkrKK/1.3.1 (iPhone; iOS 13.5; Scale/2.00)',
+        'content-type': 'application/x-www-form-urlencoded',
+        'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
+    }
 
 def f(g, d, e):
     h = pyaes.AESModeOfOperationCBC(d, iv=e)
@@ -49,7 +42,7 @@ skr = ''
 if j.status_code == 200:
     k = j.text.strip()
     l = binascii.unhexlify(k)
-    m = f(l, d, e)
+    m = f(l, d.encode(), e.encode())
     n = json.loads(m)
     for o in n['data']:
         p = f"aes-256-cfb:{o['password']}@{o['ip']}:{o['port']}"
